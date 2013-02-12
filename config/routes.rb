8b028b560e7d3fb0f297bfec3b131f3c00f9fd35
rubynovich <roman@shipiev.me>
2013-02-12 11:46:20 +0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 if Rails::VERSION::MAJOR >= 3
   RedmineApp::Application.routes.draw do
     resources :colleagues
-    connect 'projects/:project_id/colleagues', :controller => :colleagues
+    match 'projects/:project_id/colleagues' => "colleagues#index"
 #    connect 'projects/:project_id/colleagues/:id', :controller => :colleagues
   end
 else
   ActionController::Routing::Routes.draw do |map|
     map.resources :colleagues
-    map.connect 'projects/:project_id/colleagues', :controller => :colleagues
+    map.connect 'projects/:project_id/colleagues', :controller => :colleagues, :action => :index
 #    map.connect 'projects/:project_id/colleagues/:id', :controller => :colleagues
   end
 end
